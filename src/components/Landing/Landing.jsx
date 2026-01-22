640f30e756b5a3e0067430362830a9f53313ede3
Ami <ami.dorsey@me.com>
2026-01-21 19:14:49 -0500

    changed word choice

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,7 +1,7 @@
 const Landing = () => {
   return (
     <main>
-      <h1>Some thoughts are too sacred for the noise of the world. This is a safe place to whisper them back to yourself.</h1>
+      <h1>Some thoughts are too sacred for the noise of the world. This is a safe haven to whisper them back to yourself.</h1>
       <p>Sign in to continue your conversation or sign up to start your first whisper.</p>
     </main>
   );
